c5b1cfe5e4c9ecf6df1f70853b8ae1697e8e449c
Andre Almeida <aalmeida@thoughtworks.com>
2018-07-05 13:25:48 -0300

    create test with jest

--- test/firstTest.js
+++ test/firstTest.js
@@ -1,10 +1,6 @@
-import { expect } from 'chai'
-import first from '../src/first'
-//var first = require('../src/first')
 
-describe('first', () => {
-  it('first have be called', () => {
-    expect(first).to.have.been.called
+describe('test', () => {
+  it('my first test', () => {
   })
 
 })
